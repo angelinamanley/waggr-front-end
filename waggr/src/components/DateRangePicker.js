839ee0cbffe7bc25cd4c473023fa32d9e5c44b07
github username <github email address>
2019-11-14 12:59:52 +0000

    date range function and buttons

---
+++ waggr/src/components/DateRangePicker.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { DateRange} from 'react-date-range'
+import moment from 'moment'
+
+
+class DateRangePicker extends React.Component{
+
+    state = {
+        range: ""
+    }
+
+    handleSelect = range => { 
+
+        console.log(range)
+        this.setState({ startDate: moment(range.startDate).format('LLL'), endDate: moment(range.endDate).format('LLL')}, ()=> this.props.setDateRange(this.state.startDate, this.state.endDate))
+    }
+
+
+    render() { 
+    
+        return( 
+
+            <DateRange
+            
+            
+            onClose= {this.handleSelect}
+            onChange={this.handleSelect}
+
+            />
+
+
+        )
+
+    }
+
+
+}
+
+export default DateRangePicker
